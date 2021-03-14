865eb40b804329df3d837baded40239072130255
Maurice Freund <maurice.freund009@stud.fh-dortmund.de>
2021-03-14 16:40:14 +0100

    Changes to how the character list is handled * Instead of mutating the list each character keeps track of whether or not they are active * The character list can be sorted via button click

--- src/components/CharacterCardComponent.tsx
+++ src/components/CharacterCardComponent.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { Character } from '../model/character'
 
 interface CharacterCardComponentProps {
@@ -9,9 +9,13 @@ interface CharacterCardComponentProps {
 export default function CharacterCardComponent(props: CharacterCardComponentProps) {
 
   const {character} = props
+
+  const bgColor = useMemo(() => {
+    return character.isCharacterActive ? "bg-green-500" : "bg-white"
+  }, [character.isCharacterActive])
   
   return (
-    <div className="bg-white shadow-lg p-3 flex justify-center">
+    <div className={bgColor + " shadow-lg p-3 flex justify-center"}>
       <div className="w-4/6">
         <div className="flex justify-between">
           <span className="font-bold">Name: </span>
--- src/components/TurnTrackerMainComponent.tsx
+++ src/components/TurnTrackerMainComponent.tsx
@@ -8,42 +8,58 @@ import TurnOrderListComponent from './TurnOrderListComponent'
 export default function TurnTrackerMainComponent() {
 
   const initialCharacter: Character[] = [
-    {name: "Triborya", initiativeRoll: 7},
-    {name: "Herk", initiativeRoll: 14},
-    {name: "Lucas", initiativeRoll: 1}
+    {name: "Triborya", initiativeRoll: 7, isCharacterActive: true},
+    {name: "Herk", initiativeRoll: 14, isCharacterActive: false},
+    {name: "Lucas", initiativeRoll: 1, isCharacterActive: false}
   ]
 
   const [characters, setCharacters] = useState<Character[]>(initialCharacter)
 
   function pushNewCharacter() {
-    const newCharacter = {name: "Charactername", initiativeRoll: 10 }
+    const newCharacter = {name: "Charactername", initiativeRoll: 10, isCharacterActive: false}
     setCharacters([...characters, newCharacter])
   }
 
-  function rollForwards() {
-    if (characters.length > 0) {
-      const [firstCharacter, ...rest] = characters
-      setCharacters([...rest, firstCharacter])
-    }
+  function findNextValidIndexOfCharacterList(currentIndex: number, listLength: number) {
+    return currentIndex < listLength - 1 ? currentIndex + 1 : 0
   }
 
-  function rollBackwards() {
-    if (characters.length > 0) {
-      const lastCharacter = characters.pop()
-      const rest = characters.slice(0, characters.length)
-      setCharacters([lastCharacter!, ...rest])
-    }
+  function findPreviousValidIndexOfCharacterList(currentIndex: number, listLength: number) {
+    return currentIndex > 0 ? currentIndex - 1 : listLength - 1
+  }
+
+  function moveActiveCharacterToken(moveForwards: boolean) {
+    const activeCharacterIndex = characters.findIndex((char) => char.isCharacterActive)
+    const nextIndex = moveForwards 
+                      ? findNextValidIndexOfCharacterList(activeCharacterIndex, characters.length)
+                      : findPreviousValidIndexOfCharacterList(activeCharacterIndex, characters.length)
+    const newCharacterList = [...characters]
+    newCharacterList[activeCharacterIndex].isCharacterActive = false
+    newCharacterList[nextIndex].isCharacterActive = true
+    setCharacters(newCharacterList)
+  }
+
+  function sortCharacterList() {
+    const sortedCharacterList = [...characters]
+    sortedCharacterList.sort((charA, charB) => charB.initiativeRoll - charA.initiativeRoll)
+    setCharacters(sortedCharacterList)
   }
 
   return (
     <div className="flex flex-col items-center gap-3 w-5/6">
       <h1 className="text-4xl w-full underline">5e Turntracker</h1>
+      <button 
+        className="bg-green-600 hover:bg-green-400 active:bg-green-800 w-full rounded-sm p-2 text-center text-white shadow"
+        onClick={sortCharacterList}
+      >
+        Sort by initiative roll
+      </button>
       <TurnOrderListComponent characters={characters}/>
       <CharacterCreationButton onClick={pushNewCharacter}/>
       <TrackerControlComponent 
         disabled={characters.length === 0}
-        onNext={rollForwards} 
-        onBack={rollBackwards}
+        onNext={() => moveActiveCharacterToken(true)} 
+        onBack={() => moveActiveCharacterToken(false)}
       />
     </div>
   )
